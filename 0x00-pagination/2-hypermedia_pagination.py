71a2527248e7bce1961193d496714eb35087efb0
Danmwihoti <danielmwihoti@gmail.com>
2024-08-15 21:16:18 +0300

    2-hypermedia_pagination.py  get_hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Simple pagination
+Hypermedia pagination
 """
+
 import csv
 import math
 from typing import List, Tuple
@@ -57,3 +58,40 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
+
+def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    """Retrieves a specific page of the dataset of popular baby names
+        along with pagination metadata.
+
+        Args:
+            page (int, optional): The number of the page to retrieve. Defaults
+            to 1.
+            page_size (int, optional): The number of items per page. Defaults
+            to 10.
+    """
+    data = self.get_page(page, page_size)
+    if not data:
+        page_size = 0
+        total_pages = 0
+        next_page = None
+        prev_page = None
+    else:
+        page_size = min(page_size, len(self.dataset())) if page_size > 0 \
+            else len(self.dataset())
+
+        total_pages = len(self.dataset()) //\
+            page_size + (len(self.dataset()) % page_size != 0)
+
+        next_page = page + 1 if page * page_size < len(self.dataset()) \
+            else None
+        prev_page = page - 1 if page > 1 else None
+    return {
+        'page_size': page_size,
+        'page': page,
+        'data': data,
+        'next_page': next_page,
+        'prev_page': prev_page,
+        'total_pages': total_pages
+
+    }
