cb4d697b1908fdb185fc0404a5f638bbfa0e24b0
Danmwihoti <danielmwihoti@gmail.com>
2024-08-15 15:41:59 +0300

    Initialize alx-backend repo

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function index_range that takes two integer arguements page and page_size
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    function index_range
+    Args:
+        page : int,
+        page_size: int
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,12 @@
+# Project title
+0x00. Pagination
+
+## author 
+Mwihoti
+
+
+## project description
+Without pagination, a simple search could return millions or even billions of hits causing extraneous network traffic.
+
+Paging requires an implied ordering. By default this may be the item’s unique identifier, but can be other ordered fields such as a created date.
+
